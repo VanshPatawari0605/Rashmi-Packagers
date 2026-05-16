ba747f2935919222272224e1f8d7e5f68c714de5
VanshPatawari0605 <vanshpatawari@gmail.com>
2026-05-16 09:37:34 +0530

    fix: create data folder automatically

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from config import Config
+import os
 
 db = SQLAlchemy()
 login_manager = LoginManager()
@@ -9,6 +10,9 @@ login_manager = LoginManager()
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+
+    # Create data folder if it doesn't exist
+    os.makedirs(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data'), exist_ok=True)
 
     db.init_app(app)
     login_manager.init_app(app)
@@ -19,5 +23,5 @@ def create_app():
 
     with app.app_context():
         db.create_all()
-    
+
     return app
